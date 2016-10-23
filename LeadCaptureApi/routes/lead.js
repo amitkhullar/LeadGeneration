a4ae761d006a6bca4033bd38824453e07491b509
Amit Khullar <amit.khullar@blippar.com>
2016-10-24 02:00:32 +0530

    get contacts while fetching a lead

--- LeadCaptureApi/routes/lead.js
+++ LeadCaptureApi/routes/lead.js
@@ -24,20 +24,20 @@ router.route('/leads/:id')
 .get((req,res) => {
     console.log("request lead with id :"+req.params.id);
 
-    var result = {"lead1":{},"contacts1":[]};
+    // var result = {"lead1":{},"contacts1":[]};
 
-    Contact.collection.find({"lead":req.params.id},function(err,contacts){
+    Contact.find({"lead":req.params.id},function(err,contacts){
 
 
       console.log("contacts returned "+contacts);
-      result.contacts1 = contacts;
+      // result.contacts1 = contacts;
 
       Lead.findById(req.params.id, (err, lead) => {
         if (err){
           return res.send(err);
         }
-        result.lead1 = lead;
-        res.json(result);
+        // result.lead1 = lead;
+        res.json({"lead":lead,"contacts":contacts});
 
       });
 
