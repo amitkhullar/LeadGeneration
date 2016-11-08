0988547b070f02a88e7a986a5ab3692fa2dac628
Amit Khullar <amit.khullar@blippar.com>
2016-11-08 10:38:56 +0530

    DB config changes

--- LeadCaptureApi/configs/database.js
+++ LeadCaptureApi/configs/database.js
@@ -12,7 +12,7 @@ const dbName = 'LeadOrganizerApi';
 mongoose.Promise = global.Promise;
 
 // connect to the database
-mongoose.connect(`mongodb://localhost/${dbName}`);
+mongoose.connect(`mongodb://localhost:27017/${dbName}`);
 
 // get notified if the connection
 // was successful or not
